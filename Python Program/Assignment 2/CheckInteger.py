d5b1a018a83c05f4b5feb3b1bfbcfd91b3cda808
Mayu02427 <kalemayuri113@gmail.com>
2025-04-06 23:49:01 +0530

    Delete Python Program directory

--- Python Program/Assignment 2/CheckInteger.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Enter an integer: "))
-if num % 2 == 0:
-    print(f"The number {num} is Even.")
-else:
-    print(f"The number {num} is Odd.")
--- Python Program/Assignment 2/SumOfInteger.py
+++
@@ -1,9 +0,0 @@
-# Step 1: Initialize sum variable
-total_sum = 0
-
-# Step 2: Use a for loop to iterate from 1 to 50
-for num in range(1, 51):
-    total_sum += num  # Add each number to total_sum
-
-# Step 3: Display the final sum
-print("The sum of numbers from 1 to 50 is:", total_sum)
