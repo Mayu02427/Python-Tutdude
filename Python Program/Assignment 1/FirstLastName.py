e2a032998b8a0d974de8cedc246daac18cd6eae8
Mayu02427 <kalemayuri113@gmail.com>
2025-04-09 09:56:05 +0530

    Add files via upload

---
+++ Python Program/Assignment 1/FirstLastName.py
@@ -0,0 +1,9 @@
+# Taking first name and last name as input from the user
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenating the full name
+full_name = first_name + " " + last_name
+
+# Displaying a personalized greeting
+print(f"Hello, {full_name}! Welcome to the Python world ")
